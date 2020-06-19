188a66f25188257916f4b7e938938e6a4f96de0a
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-06-19 14:46:25 +0300

    Add project files.

---
+++ src/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Dungeon {
+    /// <summary>
+    /// Application main class.
+    /// </summary>
+    public static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main() {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        }
+    }
+}
